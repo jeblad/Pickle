8c734161c08bbdba781832a7154c200f47530f92
John Erling Blad <jeblad@gmail.com>
2016-07-01 15:30:38 +0200

    Cleaned up

--- Spec.php
+++ Spec.php
@@ -1,10 +1,10 @@
 <?php
 
 if ( function_exists( 'wfLoadExtension' ) ) {
-	wfLoadExtension( 'Spec' );
+	wfLoadExtension( 'Spec', __DIR__ . '/extension.json' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['Spec'] = __DIR__ . '/i18n';
-	
+
 	$wgExtensionMessagesFiles['SpecMagic'] = __DIR__ . '/Spec.i18n.magic.php';
 	wfWarn(
 		'Deprecated PHP entry point used for Spec extension. Please use wfLoadExtension ' .
