efa1bb68d75353262b435f289157a3411c289b9f
John Erling Blad <jeblad@gmail.com>
2017-03-18 23:26:43 +0100

    Added tests for exported aliases

--- tests/phpunit/includes/LuaLibrary/lua/non-pure/PickleTest.lua
+++ tests/phpunit/includes/LuaLibrary/lua/non-pure/PickleTest.lua
@@ -25,6 +25,10 @@ local function testCall( ... ) -- luacheck: ignore
 	return type( subject ), type( expect ) -- luacheck: globals subject expect
 end
 
+local function testMWFunc( nme ) -- luacheck: ignore
+	return type( mw[ nme ] )
+end
+
 local tests = {
 	{
 		name = 'pickles exists',
@@ -49,6 +53,60 @@ local tests = {
 		func = testCall,
 		args = { },
 		expect = { 'table', 'table' }
+	},
+	{
+		name = 'table mw.describe',
+		func = testMWFunc,
+		args = { 'describe' },
+		expect = { 'table' }
+	},
+	{
+		name = 'table mw.context',
+		func = testMWFunc,
+		args = { 'context' },
+		expect = { 'table' }
+	},
+	{
+		name = 'table mw.it',
+		func = testMWFunc,
+		args = { 'it' },
+		expect = { 'table' }
+	},
+	{
+		name = 'table mw.subject',
+		func = testMWFunc,
+		args = { 'subject' },
+		expect = { 'table' }
+	},
+	{
+		name = 'table mw.expect',
+		func = testMWFunc,
+		args = { 'expect' },
+		expect = { 'table' }
+	},
+	{
+		name = 'table mw.carp',
+		func = testMWFunc,
+		args = { 'carp' },
+		expect = { 'function' }
+	},
+	{
+		name = 'table mw.carp',
+		func = testMWFunc,
+		args = { 'cluck' },
+		expect = { 'function' }
+	},
+	{
+		name = 'table mw.carp',
+		func = testMWFunc,
+		args = { 'confess' },
+		expect = { 'function' }
+	},
+	{
+		name = 'table mw.carp',
+		func = testMWFunc,
+		args = { 'croak' },
+		expect = { 'function' }
 	},
 }
 
