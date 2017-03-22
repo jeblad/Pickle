b0b3757a40ca9eddfe775de4cf9aea8158b921c3
John Erling Blad <jeblad@gmail.com>
2017-03-22 23:13:52 +0100

    Documented renders

--- includes/LuaLibrary/lua/pure/picklelib/render/Renders.lua
+++ includes/LuaLibrary/lua/pure/picklelib/render/Renders.lua
@@ -4,6 +4,8 @@
 local Renders = {}
 
 --- Lookup of missing class members
+-- @param string used for lookup of member
+-- @return any
 function Renders:__index( key ) -- luacheck: no self
 	return Renders[key]
 end
@@ -13,6 +15,7 @@ Renders._styles = {}
 
 --- Convenience function to access a specific named style
 -- This will try to create the style if it isn't created yet.'
+-- @return Render
 function Renders:__call( name ) -- luacheck: no self
 	assert( name, 'Failed to provide a name' )
 	assert( Renders._styles[name], 'Failed to provide a previously registered style' )
@@ -22,6 +25,8 @@ end
 --- Register named style
 -- This is at class level. It is really a two level strategy, but we're lazy
 -- and skip one of the levels.
+-- @param string name of a style of rendering
+-- @return Render
 function Renders.registerStyle( name )
 	if not Renders._styles[name] then
 		Renders._styles[name] = Renders.create( name )
@@ -30,6 +35,8 @@ function Renders.registerStyle( name )
 end
 
 --- Create a new instance
+-- @param name of a style of rendering
+-- @return self
 function Renders.create( name )
 	local self = setmetatable( {}, Renders )
 	self:_init( name )
@@ -37,6 +44,8 @@ function Renders.create( name )
 end
 
 --- Initialize a new instance
+-- @private
+-- @param string name of a style of rendering
 function Renders:_init( name )
 	assert( name, 'Failed to provide a name' )
 	self._style = name
@@ -45,6 +54,9 @@ end
 
 --- Register a render of given named type
 -- This will typically be "Result" or "Report".
+-- @param string name of a style of rendering
+-- @param Render lib for the specific kind of rendering
+-- @return Render
 function Renders:registerType( name, lib )
 	assert( name, 'Failed to provide a name' )
 	self._types[name] = lib
@@ -53,10 +65,13 @@ end
 
 --- Find render of the correct named type
 -- This will typically be "Result" or "Report".
+-- @todo check if this has tests
+-- @param string name of a style of rendering
+-- @return Render
 function Renders:find( name )
 	assert( name, 'Failed to provide a name' )
-	assert( self._types[type], 'Failed to provide a previously registered type' )
-	return self._types[type]
+	assert( self._types[name], 'Failed to provide a previously registered type' )
+	return self._types[name]
 end
 
 -- Return the final class
