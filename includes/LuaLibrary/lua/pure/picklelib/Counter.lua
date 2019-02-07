f281e705f45a65bc09625205d99447ce3c07fb06
John Erling Blad <jeblad@gmail.com>
2019-02-07 16:02:01 +0100

    Updated Counter.lua

--- includes/LuaLibrary/lua/pure/picklelib/Counter.lua
+++ includes/LuaLibrary/lua/pure/picklelib/Counter.lua
@@ -1,46 +1,56 @@
--- Class for the running count
+--- Counter for a continuous incrementing number.
+-- @classmod Counter
 
 -- @var class for the lib
 local Counter = {}
 
---- Lookup of missing class members
+--- Lookup of missing class members.
+-- @param key string used for lookup of member
+-- @return any
 function Counter:__index( key ) -- luacheck: no self
 	return Counter[key]
 end
 
---- Call on instance
+--- Call on instance.
+-- Increment value, but return the old value.
+-- @return previous value
 function Counter:__call() -- luacheck: no self
 	local prev = self:num() or 0
 	self:inc()
 	return prev
 end
 
---- Create a new instance
+--- Create a new instance.
+-- @param num initial value
+-- @return self
 function Counter.create( num )
 	local self = setmetatable( {}, Counter )
 	self:_init( num )
 	return self
 end
 
---- Initialize a new instance
+--- Initialize a new instance.
+-- @local
+-- @param num initial value
+-- @return self
 function Counter:_init( num )
 	self._num = num or false
 	return self
 end
 
---- Is the running count initialized
+--- Is the running count initialized.
 -- @return boolean saying whether the internal structure has been initialized
 function Counter:isInitialized()
 	return not not self._num
 end
 
---- The current value
+--- The current value.
 -- @return int current value
 function Counter:num()
 	return self._num
 end
 
---- Increment the value
+--- Increment the value.
 -- @return int incremented value
 function Counter:inc()
 	if not self._num then
@@ -50,5 +60,5 @@ function Counter:inc()
 	return self._num
 end
 
--- Return the final class
+-- Return the final class.
 return Counter
