983969dcafb9e832c98f4524c42830590e2ec90b
John Erling Blad <jeblad@gmail.com>
2019-02-07 18:07:51 +0100

    Updated TranslatorStrategiesTest.lua

--- tests/phpunit/includes/LuaLibrary/lua/pure/picklelib/translator/TranslatorStrategiesTest.lua
+++ tests/phpunit/includes/LuaLibrary/lua/pure/picklelib/translator/TranslatorStrategiesTest.lua
@@ -1,4 +1,4 @@
---- Tests for the translator strategies module
+--- Tests for the translator strategies module.
 -- This is a preliminary solution
 -- @license GNU GPL v2+
 -- @author John Erling Blad < jeblad@gmail.com >
