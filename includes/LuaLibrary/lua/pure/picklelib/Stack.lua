9c1c4f02b7ee8a94dec6e5cc97023817cddebe44
John Erling Blad <jeblad@gmail.com>
2019-02-07 16:02:20 +0100

    Updated Stack.lua

--- includes/LuaLibrary/lua/pure/picklelib/Stack.lua
+++ includes/LuaLibrary/lua/pure/picklelib/Stack.lua
@@ -1,27 +1,36 @@
---- Class for a table handled as a stack
+--- Stack for managing values.
+-- @classmod Stack
 
+-- @var class for the lib
 local Stack = {}
 
---- Lookup of missing class members
+--- Lookup of missing class members.
+-- @param key string used for lookup of member
+-- @return any
 function Stack:__index( key ) -- luacheck: no self
 	return Stack[key]
 end
 
---- Create a new instance
+--- Create a new instance.
+-- @param ... varargs pushed on the stack
+-- @return self
 function Stack.create( ... )
 	local self = setmetatable( {}, Stack )
 	self:_init( ... )
 	return self
 end
 
---- Initialize a new instance
+--- Initialize a new instance.
+-- @local
+-- @param ... varargs pushed on the stack
+-- @return self
 function Stack:_init( ... )
 	self._stack = {}
 	self:push( ... )
 	return self
 end
 
---- Is the stack empty
+--- Is the stack empty.
 -- Note that the internal structure is non-empty even if a nil
 -- is pushed on the stack.
 -- @return boolean saying whether the internal structure has length zero
@@ -29,7 +38,7 @@ function Stack:isEmpty()
 	return #self._stack == 0
 end
 
---- What is the depth of the internal structure
+--- What is the depth of the internal structure.
 -- Note that the internal structure has a depth even if a nil
 -- is pushed on the stack.
 -- @return number saying how deep the internal structure is
@@ -37,7 +46,7 @@ function Stack:depth()
 	return #self._stack
 end
 
---- Get the layout of the stack
+--- Get the layout of the stack.
 -- This method is used for testing to inspect which types of objects
 function Stack:layout()
 	local t = {}
@@ -47,7 +56,7 @@ function Stack:layout()
 	return t
 end
 
---- Get a reference to the bottommost item in the stack
+--- Get a reference to the bottommost item in the stack.
 -- The bottommost item can also be described as the first item.
 -- This method leaves the item on the stack.
 -- @alias first
@@ -57,7 +66,7 @@ function Stack:bottom()
 end
 Stack.first = Stack.bottom
 
---- Get a reference to the topmost item in the stack
+--- Get a reference to the topmost item in the stack.
 -- The topmost item can also be described as the last item.
 -- This method leaves the item on the stack.
 -- @alias last
@@ -67,7 +76,7 @@ function Stack:top()
 end
 Stack.last = Stack.top
 
---- Push a value on the stack
+--- Push a value on the stack.
 -- @return self so chaing is unbroken
 function Stack:push( ... )
 	for _,v in ipairs( { ... } ) do
@@ -76,47 +85,47 @@ function Stack:push( ... )
 	return self
 end
 
---- Pop the last value of the stack
+--- Pop the last value of the stack.
 -- Note that this will remove the last (topmost) value.
--- @param n number of items to drop
+-- @param num items to drop
 -- @return any item that can be put on the stack
-function Stack:pop( n )
-	local a = {}
+function Stack:pop( num )
+	local t = {}
 
-	for i=1,(n or 1) do
-		a[(n or 1) - i + 1] = table.remove( self._stack )
+	for i=1,(num or 1) do
+		t[(num or 1) - i + 1] = table.remove( self._stack )
 	end
 
-	return unpack( a )
+	return unpack( t )
 end
 
---- Drop the last n values of the stack
+--- Drop the last n values of the stack.
 -- Note that this will remove the last (topmost) values.
--- @param n number of items to drop
+-- @param num of items to drop
 -- @return self so chaing is unbroken
-function Stack:drop( n )
-	for _=1,(n or 1) do
+function Stack:drop( num )
+	for _=1,(num or 1) do
 		table.remove( self._stack )
 	end
 
 	return self
 end
 
---- Get the indexed entry
+--- Get the indexed entry.
 -- Accessing this will not change stored values.
--- @param n entry number, negative numbers count backwards
+-- @param num entry number, negative numbers count backwards
 -- @return stored value
-function Stack:get( n )
-	assert( n ~= 0 )
-	if n < 0 then
+function Stack:get( num )
+	assert( num ~= 0 )
+	if num < 0 then
 		-- n is negative, and add 1 to hit the end point
-		return self._stack[#self._stack + n + 1]
+		return self._stack[#self._stack + num + 1]
 	end
 	-- note n is positive
-	return self._stack[n]
+	return self._stack[num]
 end
 
---- Export a list of all the contents
+--- Export a list of all the contents.
 -- @return list of values
 function Stack:export()
 	local t = {}
@@ -128,7 +137,7 @@ function Stack:export()
 	return unpack( t )
 end
 
---- Flush all the contents
+--- Flush all the contents.
 -- Note that this clears the internal storage.
 -- @return list of values
 function Stack:flush()
@@ -139,5 +148,5 @@ function Stack:flush()
 	return unpack( t )
 end
 
--- Return the final class
+-- Return the final class.
 return Stack
