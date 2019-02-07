5ac6139f9655b8d83e506f0bebaa42bd296d50ed
John Erling Blad <jeblad@gmail.com>
2019-02-07 18:06:51 +0100

    Updated TranslatorStrategies.lua

--- includes/LuaLibrary/lua/pure/picklelib/translator/TranslatorStrategies.lua
+++ includes/LuaLibrary/lua/pure/picklelib/translator/TranslatorStrategies.lua
@@ -1,58 +1,61 @@
---- Class for extractor strategies
--- This should be a strategy pattern
+--- Class for extractor strategies.
+-- This should be a strategy pattern.
+-- @classmod TranslatorStrategies
+-- @alias Translators
 
 -- @var class var for lib
 local Translators = {}
 
---- Lookup of missing class members
--- @param string used for lookup of member
+--- Lookup of missing class members.
+-- @tparam string key lookup of member
 -- @return any
 function Translators:__index( key ) -- luacheck: no self
 	return Translators[key]
 end
 
---- Create a new instance
--- @param vararg list of strategies
--- @return self
+--- Create a new instance.
+-- @tparam vararg ... list of strategies
+-- @treturn self
 function Translators.create( ... )
 	local self = setmetatable( {}, Translators )
 	self:_init( ... )
 	return self
 end
 
---- Initialize a new instance
--- @private
--- @param vararg list of strategies
--- @return self
+--- Initialize a new instance.
+-- @local
+-- @tparam vararg ... list of strategies
+-- @treturn self
 function Translators:_init()
 	self._strategies = {}
 	return self
 end
 
---- Register a new strategy
--- @param strategy to be registered
--- @return self
-function Translators:register( str, strategy )
-	self._strategies[ str ] = strategy
+--- Register a new strategy.
+-- @tparam string key lookup of translator
+-- @tparam Translator strategy to be registered
+-- @treturn self
+function Translators:register( key, strategy )
+	self._strategies[ key ] = strategy
 	return self
 end
 
---- Removes all registered translators
--- @return self
+--- Removes all registered translators.
+-- @treturn self
 function Translators:flush()
 	self._strategies = {}
 	return self
 end
 
---- Find a matching extractor
--- @exception On missing source
--- @param string used as the key for a translator
--- @return strategy, first, last
-function Translators:find( str )
-	assert( str )
+--- Find a matching extractor.
+-- @raise On missing source
+-- @tparam string key lookup of translator
+-- @treturn Translator,number,number strategy,first,last
+function Translators:find( key )
+	assert( key )
 
-	return self._strategies[ str ]
+	return self._strategies[ key ]
 end
 
--- Return the final class
+-- Return the final class.
 return Translators
